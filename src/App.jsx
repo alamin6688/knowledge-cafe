e663a6ddcc350849d93cc4940b83dd68bf326e0c
alamin6688 <alamin.kzs06@gmail.com>
2024-03-16 04:26:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Blogs from './Component/Header/Blogs/Blogs'
+import Header from './Component/Header/Header'
+
+function App() {
+
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+      
+      
+    </>
+  )
+}
+
+export default App
